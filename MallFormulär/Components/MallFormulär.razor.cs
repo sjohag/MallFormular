d4bcc36ec366819c63a63fb50dbb822071f3d120
Jonas Sjöhag <jonas.sjohag@gmail.com>
2020-08-04 21:23:44 +0200

    Add project files.

---
+++ MallFormulär/Components/MallFormulär.razor.cs
@@ -0,0 +1,26 @@
+﻿using MallFormulär.DTO;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MallFormulär.Components
+{
+    public partial class MallFormulär<TData> where TData : Bas, new()
+    {
+        [Parameter] public EventCallback<TData> FormulärSparat { get; set; }
+        [Parameter] public RenderFragment<TData> ChildContent { get; set; }
+        private TData Data { get; set; }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            Data = new TData();
+        }
+        private async Task Sparat()
+        {
+            await FormulärSparat.InvokeAsync(Data);
+        }
+    }
+}
---
+++ MallFormulär/DTO/Data.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MallFormulär.DTO
+{
+    public class Bas
+    {
+        [Required, StringLength(5, MinimumLength = 1)]
+        public string BasText { get; set; }
+        [Range(10, 20)]
+        public int BasTal { get; set; }
+    }
+    public class DataA:Bas
+    {
+        [Range(100,200)]
+        public int TalA { get; set; }
+    }
+    public class DataB : Bas
+    {
+        [Required, StringLength(10, MinimumLength = 2)]
+        public string TextB { get; set; }
+    }
+}
