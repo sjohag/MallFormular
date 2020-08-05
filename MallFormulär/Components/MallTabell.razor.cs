e70be0771c428aeec39356e4aacea34717187901
Jonas Sjöhag <jonas.sjohag@dekiru.se>
2020-08-05 08:10:52 +0200

    MallTabell

---
+++ MallFormulär/Components/MallTabell.razor.cs
@@ -0,0 +1,16 @@
+﻿using MallFormulär.DTO;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MallFormulär.Components
+{
+    public partial class MallTabell<TData> where TData : Bas, new()
+    {
+        [Parameter] public RenderFragment ExtraRubriker { get; set; }
+        [Parameter] public RenderFragment<TData> ExtraKolumner { get; set; }
+        [Parameter] public IEnumerable<TData> Data { get; set; }
+    }
+}
